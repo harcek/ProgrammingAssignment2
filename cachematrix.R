d5f6492add3ac26592e681832e84c50ed5aab1e0
Daniel Harcek <daniel.harcek@visualdna.com>
2014-07-26 09:27:21 +0200

    Added input check to cacheSolve. Added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module contains function which enable us to create a matrix capable of
+## storing it's inverse once computed, to be reused for next time, and resolver 
+## function which is able to work with such construct.
+## This can bring positive performance benefits. 
 
-## Write a short comment describing this function
+## This function creates a "enriched matrix" which contains utility functions
+## to get and set the matrix, and to get and set the inverse of matrix.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -16,14 +19,22 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## This function is a cachedMatrix resolver.
+## It checks if cachedMatrix passed as a input contains
+## computed inverse, otherwise it computes it, stores into cachedMatrix
+## and returns the inverse itself.
 cacheSolve <- function(x, ...) {
+  ## do we have a valid input?
+  if (!("getinverse" %in% names(x))) {
+    stop("input is not valid cachedMatrix")
+  }
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  ## retrieve, compute inverse and set
   data <- x$get()
   m <- solve(data, ...)
   x$setinverse(m)
